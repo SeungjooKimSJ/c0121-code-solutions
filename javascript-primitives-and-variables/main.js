089a8ce3b9eeb82d0926a308fafbe0f86aa0edb7
Seungjoo Kim <sjskybone@gmail.com>
2021-01-25 10:33:28 -0800

    Completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,31 @@
+const firstName = 'Seungjoo';
+const lastName = 'Kim';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+
+const isCool = fullName === 'Seungjoo Kim';
+console.log('value of isCool:', isCool);
+
+const totalPets = 1;
+console.log('value of totalPets:', totalPets);
+
+const fullNameType = typeof fullName;
+console.log('typeof fullName:', fullNameType);
+
+const isCoolType = typeof isCool;
+console.log('typeof isCool:', isCoolType);
+
+const totalPetsType = typeof totalPets;
+console.log('typeof totalPets:', totalPetsType);
+
+const unknown = null;
+console.log('value of unknown:', unknown);
+
+const unknownType = typeof unknown;
+console.log('typeof unknown:', unknownType);
+
+let never;
+console.log('value of never:', never);
+
+const neverTypeof = typeof never;
+console.log('type of never:', neverTypeof);
