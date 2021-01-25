b2deb33d863eeae5fd2682fd0cccfbb93520029d
Seungjoo Kim <sjskybone@gmail.com>
2021-01-25 12:49:48 -0800

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+const width = 5;
+const height = 10;
+const area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 100;
+const payment = 1000;
+const change = payment - bill;
+
+console.log('value of change', change);
+console.log('typeof change:', typeof change);
+
+const quizzes = 98;
+const midterm = 92;
+const final = 95;
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Seungjoo';
+const lastName = 'Kim';
+const fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 8;
+const isAcidic = pH > 7;
+// var acidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 200;
+const isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+const motto = fullName + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
