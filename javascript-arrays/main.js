786b8f5e563f701e93faef02664224d472b15860
Seungjoo Kim <sjskybone@gmail.com>
2021-01-25 17:50:19 -0800

    Completed javascript-arrays.

--- javascript-arrays/main.js
+++ javascript-arrays/main.js
@@ -1,4 +1,4 @@
-const colors = ['red', 'white', 'blue'];
+var colors = ['red', 'white', 'blue'];
 
 console.log('value of colors[0]:', colors[0]);
 console.log('value of colors[1]:', colors[1]);
@@ -10,13 +10,13 @@ colors[2] = 'green';
 console.log('Mexico is ' + colors[0] + ', ' + colors[1] + ', and ' + colors[2] + '.');
 console.log('value of colors:', colors);
 
-const students = ['Leonardo', 'Donatello', 'Raphael', 'Michelangelo'];
-const numberOfStudents = students.length;
+var students = ['Leonardo', 'Donatello', 'Raphael', 'Michelangelo'];
+var numberOfStudents = students.length;
 
 console.log('There are ' + numberOfStudents + ' students in the class.');
 
-const lastIndex = numberOfStudents - 1;
-const lastStudent = students[lastIndex];
+var lastIndex = numberOfStudents - 1;
+var lastStudent = students[lastIndex];
 
 console.log('The last student in the array is ' + lastStudent + '.');
 console.log('value of students:', students);
