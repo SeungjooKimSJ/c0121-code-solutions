4e2b189b063ad7411662568ba185e9ac7ed397d6
Seungjoo Kim <sjskybone@gmail.com>
2021-02-02 23:54:09 -0800

    Completed javascript-modal.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,28 +1,14 @@
-var $trigger = document.querySelector('.trigger');
+var $trigger = document.querySelector('#trigger');
 var $modalContent = document.querySelector('.modal-content');
 var $no = document.querySelector('.no');
+var $active = document.querySelector('.active');
 
 $trigger.addEventListener('click', function (event) {
-  $modalContent.className = 'modal-content ' + 'on';
-  // $modalContent.className = 'modal-content ' + 'off';
+  $modalContent.className = 'modal-content ' + 'off';
+  $active.className = 'active ' + 'off';
 });
 
 $no.addEventListener('click', function (event) {
   $modalContent.className = 'modal-content ' + 'on';
+  $active.className = 'active ' + 'on';
 });
-
-// var $modalOff = document.querySelector('.modal-content.off');
-
-// var clickBtn = 0;
-
-// $trigger.addEventListener('click', function (event) {
-//   clickBtn++;
-//   var popUp;
-
-//   if (clickBtn % 2 === 0) {
-//     popUp = 'off';
-//   } else {
-//     popUp = 'on';
-//   }
-//   $modalContent.className = 'modal-content ' + popUp;
-// });
