cc760bea0de6b2fc4ed1474d7b54b047fc8fbc73
Seungjoo Kim <sjskybone@gmail.com>
2021-02-02 23:57:18 -0800

    Delete commented out code.

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -1,11 +1,4 @@
 /* exported filterOutNulls */
-// function filterOutNulls(values) {
-//   for (var i = values.length - 1; i >= 0; i--) {
-//     if (values[i] === null) { values.splice(i, 1); }
-//   }
-//   return values;
-// }
-
 function filterOutNulls(values) {
   var newArr = [];
   for (var i = 0; i < values.length; i++) {
