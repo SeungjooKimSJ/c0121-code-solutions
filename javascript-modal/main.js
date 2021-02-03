38801fab13bf536f5b13b7b62ce20aca571d723f
Seungjoo Kim <sjskybone@gmail.com>
2021-02-02 18:05:12 -0800

    Inprogress javascript-modal.

---
+++ javascript-modal/main.js
@@ -0,0 +1,17 @@
+
+// var clickedBtn = 0;
+// var $btn = document.querySelector('.btn');
+// var $main = document.querySelector('.main');
+
+// $main.addEventListener('click', function (event) {
+//   clickedBtn++;
+//   var popUp;
+
+//   if (clickedBtn > 0) {
+//     popUp = 'off';
+//   } else {
+//     popUp = 'on';
+//   }
+//   $btn.className = 'btn ' + popUp;
+//   $main.textContent = 'Would you like to take a survey?';
+// });
