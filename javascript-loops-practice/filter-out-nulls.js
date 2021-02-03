159d17d40909c5985484bbbb502de464386b28ef
Seungjoo Kim <sjskybone@gmail.com>
2021-02-02 18:34:47 -0800

    Delete commented out.

--- javascript-loops-practice/filter-out-nulls.js
+++ javascript-loops-practice/filter-out-nulls.js
@@ -1,7 +1,17 @@
 /* exported filterOutNulls */
+// function filterOutNulls(values) {
+//   for (var i = values.length - 1; i >= 0; i--) {
+//     if (values[i] === null) { values.splice(i, 1); }
+//   }
+//   return values;
+// }
+
 function filterOutNulls(values) {
-  for (var i = values.length - 1; i >= 0; i--) {
-    if (typeof values[i] === 'object') { values.splice(i, 1); }
+  var newArr = [];
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] !== null) {
+      newArr.push(values[i]);
+    }
   }
-  return values;
+  return newArr;
 }
