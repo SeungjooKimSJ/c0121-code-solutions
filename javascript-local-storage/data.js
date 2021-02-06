17fba4aea80f9cd3d0121575dbb11acce18272f1
Seungjoo Kim <sjskybone@gmail.com>
2021-02-05 17:57:23 -0800

    Completed javascript-local-storage.

--- javascript-local-storage/data.js
+++ javascript-local-storage/data.js
@@ -3,9 +3,12 @@
 var todos = [];
 
 var previousTodosJSON = localStorage.getItem('javascript-local-storage');
-if (previousTodosJSON !== null) { todos = JSON.parse(previousTodosJSON); }
 
-window.addEventListener('beforeunload', function () {
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+window.addEventListener('beforeunload', function (event) {
   var todosJSON = JSON.stringify(todos);
   localStorage.setItem('javascript-local-storage', todosJSON);
 });
