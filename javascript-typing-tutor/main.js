62381784430c0d1d8ec90c08ad6e65816e89b5cb
Seungjoo Kim <sjskybone@gmail.com>
2021-02-08 23:59:11 -0800

    Completed javascript-typing-tutor.

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,28 +1,3 @@
-// var $typing = document.querySelector('.typing');
-// var $quoteDisplay = document.querySelector('.quote-display');
-
-// var $h1Text = 'grumpy wizards make toxic brew';
-
-// var $characters = $h1Text.split('').map(function (word) {
-//   var $span = document.createElement('span');
-//   $span.textContent = word;
-//   $quoteDisplay.appendChild($span);
-
-//   return $span;
-// });
-
-// var cursorIndex = 0;
-// var $cursorCharacter = $characters[0];
-
-// document.addEventListener('keydown', function ({ key }) {
-//   if (key === $cursorCharacter.textContent) {
-//     $cursorCharacter.className = 'done';
-//     $cursorCharacter = $characters[++cursorIndex];
-//   } else {
-//     $cursorCharacter.className = 'cursor';
-//   }
-// });
-
 var $quoteString = 'grumpy wizards make toxic brew';
 var $quoteDisplay = document.querySelector('.quote-display');
 
