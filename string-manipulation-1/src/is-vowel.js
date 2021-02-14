583beceadacfb8b66c759c6b1072f773c16cc800
Seungjoo Kim <sjskybone@gmail.com>
2021-02-14 00:18:32 -0800

    Completed string-manipulation-1.

--- string-manipulation-1/src/is-vowel.js
+++ string-manipulation-1/src/is-vowel.js
@@ -1,18 +1,10 @@
 /* exported isVowel */
 function isVowel(char) {
-  var vowel = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'];
-
-  for (var i = 0; i < char.length; i++) {
-    if (char === vowel[i]) {
-      return true;
-    } else {
-      return false;
-    }
+  if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+    return true;
+  } else if (char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+    return true;
+  } else {
+    return false;
   }
-
-  // if (char === 'a' || char === 'e' | char === 'i' || char === 'o' | char === 'u') {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
 }
