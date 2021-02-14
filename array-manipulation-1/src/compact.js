c20ee36648fce39d357f45605da1b91e6d283f9a
Seungjoo Kim <sjskybone@gmail.com>
2021-02-14 01:08:57 -0800

    Only last question is left for array-manipulation-1.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,10 +1,10 @@
 /* exported compact */
 function compact(array) {
-  // var omits = [false, null, NaN, 0, undefined, ''];
+  var omits = [false, null, NaN, 0, undefined, ''];
 
-  // for (var i = 0; i < array.length; i++) {
-  //   // array[i].filter(omits);
-  // }
-  // return array;
+  for (var i = 0; i < array.length; i++) {
+    array[i].filter(omits);
+  }
+  return array;
 
 }
