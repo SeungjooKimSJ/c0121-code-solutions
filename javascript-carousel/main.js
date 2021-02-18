b67c5f951e9ad44c885bd70a443f3c3af707707f
Seungjoo Kim <sjskybone@gmail.com>
2021-02-18 00:03:29 -0800

    Have to make a function for dots javascript-carousel.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,6 +1,5 @@
 var $pictures = document.querySelectorAll('.pictures');
-var $images = document.querySelectorAll('.images');
-var $dots = document.querySelectorAll('.dots');
+// var $dots = document.querySelectorAll('.dots');
 
 var $arrowsLeft = document.querySelector('.arrows-left');
 var $arrowsRight = document.querySelector('.arrows-right');
@@ -27,20 +26,19 @@ $arrowsRight.addEventListener('click', function () {
   $pictures[currentPics].className = 'pictures active';
 });
 
+function autoSlide() {
+  $pictures[currentPics].className = 'pictures inactive';
+  currentPics = currentPics + 1;
+
+  if (currentPics > 4) {
+    currentPics = 0;
+  }
+  $pictures[currentPics].className = 'pictures active';
+}
+
+setInterval(autoSlide, 3000);
+
 // $dots.addEventListener('click', function () {
-
-// })
-
 // event.target === $dots[i];
-// logic !!!!!!!!!!
-
-
-
-// function slideShow() {
-//   currentPics++;
-//   if (currentPics === 4) {
-//     return currentPics;
-//   }
-// };
-
-// var timerID = setInterval(slideShow, 3000);
+// and then logic !!!!!!!!!!
+// })
