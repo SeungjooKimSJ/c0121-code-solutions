250b0e32f0ce15508750663c4c2c628f405003de
Seungjoo Kim <sjskybone@gmail.com>
2021-02-18 22:00:54 -0800

    Working on Account balance function.

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -3,12 +3,12 @@ function Account(number, holder) {
   this.number = number;
   this.holder = holder;
   this.transactions = [];
-};
+}
 
 Account.prototype.deposit = function (amount) {
-  if (newAmount > 0) {
-    var newAmount = new Amount;
-    this.transactions.push(newAmount);
+  if (amount > 0) {
+    var newTransaction = new Transaction('deposit', amount);
+    this.transactions.push(newTransaction);
     return true;
   } else {
     return false;
@@ -16,18 +16,18 @@ Account.prototype.deposit = function (amount) {
 };
 
 Account.prototype.withdraw = function (amount) {
-  if (newAmount > 0) {
-    var newAmount = new Amount;
-    this.transactions.push(newAmount);
+  if (amount > 0) {
+    var newTransaction = new Transaction('withdrawal', amount);
+    this.transactions.push(newTransaction);
     return true;
   } else {
     return false;
   }
 };
 
-Account.prototype.getBalance = function () {
+Account.prototype.getBalance = function (amount) {
   var totalAmount = 0;
-
+  // console.log('this.transactions:', this.transactions);
   for (var i = 0; i < this.transactions.length; i++) {
     if (this.transactions.type === 'deposit') {
       totalAmount += this.transactions[i];
@@ -36,4 +36,4 @@ Account.prototype.getBalance = function () {
     }
   }
   return totalAmount;
-}
+};
