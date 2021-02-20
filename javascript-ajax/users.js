0981124aa4c8dd365457f40cfb05386440314407
Seungjoo Kim <sjskybone@gmail.com>
2021-02-19 17:13:45 -0800

    Completed javascript-ajax. | Seungjoo Kim

---
+++ javascript-ajax/users.js
@@ -0,0 +1,20 @@
+var $userList = document.querySelector('#user-list');
+
+function getUserList(name) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    console.log('xhr.status:', xhr.status);
+    console.log('xhr.respone:', xhr.response);
+
+    for (var i = 0; i < xhr.response.length; i++) {
+      var $li = document.createElement('li');
+      $li.textContent = xhr.response[i].name;
+      $userList.append($li);
+    }
+  });
+  xhr.send();
+};
+
+getUserList();
