abb5c9f42a0025013b849450353eb3dcbfbd2488
Seungjoo Kim <sjskybone@gmail.com>
2021-02-19 18:08:21 -0800

    Completed javascript-ajax. | Seungjoo Kim

--- javascript-ajax/users.js
+++ javascript-ajax/users.js
@@ -1,21 +1,21 @@
 var $userList = document.querySelector('#user-list');
+var xhr = new XMLHttpRequest();
 
-function getUserList(name) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
-  xhr.responseType = 'json';
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
 
-  xhr.addEventListener('load', function () {
-    console.log('xhr.status:', xhr.status);
-    console.log('xhr.respone:', xhr.response);
+xhr.addEventListener('load', function () {
+  console.log('xhr.status:', xhr.status);
+  console.log('xhr.respone:', xhr.response);
 
-    for (var i = 0; i < xhr.response.length; i++) {
-      var $li = document.createElement('li');
-      $li.textContent = xhr.response[i].name;
-      $userList.append($li);
-    }
-  });
-  xhr.send();
-};
+  var users = xhr.response;
 
-getUserList();
+  for (var i = 0; i < users.length; i++) {
+    var user = users[i];
+    var $li = document.createElement('li');
+
+    $li.textContent = user.name;
+    $userList.appendChild($li);
+  }
+});
+xhr.send();
